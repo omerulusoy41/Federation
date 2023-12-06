af7b765be0dca7cc60be48b916c7ac59ed50d4ba
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-12-06 11:03:11 +0300

    appname change

--- src/main/java/com/ofu/federation/Repository/BookRepository.java
+++ src/main/java/com/ofu/federation/Repository/BookRepository.java
@@ -4,4 +4,5 @@ import com.ofu.federation.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BookRepository extends MongoRepository<Book,String> {
+
 }
